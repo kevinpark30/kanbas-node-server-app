8037de558570d8d6ada0da218e76dc412d48af37
kevinpark30 <cykevin30@gmail.com>
2023-11-14 17:42:04 -0500

    Finished 4.4

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import Hello from "./hello.js";
+import cors from "cors";
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
 const app = express();
+app.use(cors());
 app.use(express.json());
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
