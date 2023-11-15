c1449728bd129b58487cb1a81be0a10ba710646c
kevinpark30 <cykevin30@gmail.com>
2023-11-14 21:36:46 -0500

    Finished a5 server

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ import Hello from "./hello.js";
 import cors from "cors";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import "dotenv/config";
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+
 app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
