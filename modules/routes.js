d7e07521c45e89e2f38728c06396429fe48bc134
kevinpark30 <cykevin30@gmail.com>
2023-11-18 14:50:21 -0500

    Finished a5

--- modules/routes.js
+++ modules/routes.js
@@ -13,7 +13,7 @@ function ModuleRoutes(app) {
     app.delete("/api/modules/:mid", (req, res) => {
         const { mid } = req.params;
         db.modules = db.modules.filter((m) => m._id !== mid);
-        res.sendStatus(200);
+        res.sendStatus(204);
     });
 
     app.post("/api/courses/:cid/modules", (req, res) => {
